edd328ec1e8a3f683ef06f2474fc4c92544c225a
@hexlet/cli <support@hexlet.io>
2023-08-22 09:33:03 +0300

    submit java-spring-ru/spring-orm

---
+++ java-spring-ru/spring-orm/src/main/java/exercise/controller/PeopleController.java
@@ -0,0 +1,54 @@
+package exercise.controller;
+
+import exercise.model.Person;
+import exercise.dto.PersonDto;
+import exercise.repository.PersonRepository;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import lombok.RequiredArgsConstructor;
+
+
+@RestController
+@RequestMapping("/people")
+@RequiredArgsConstructor
+public class PeopleController {
+
+    // Автоматически заполняем значение поля
+    private final PersonRepository personRepository;
+
+    @GetMapping(path = "/{id}")
+    public Person getPerson(@PathVariable long id) {
+        return personRepository.findById(id).orElseThrow();
+    }
+
+    @GetMapping(path = "")
+    public Iterable<Person> getPeople() {
+        return this.personRepository.findAll();
+    }
+
+    // BEGIN
+    @PostMapping(path = "")
+    public void createPerson(@RequestBody Person person) {
+        this.personRepository.save(person);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public void deletePerson(@PathVariable long id) {
+        this.personRepository.delete(getPerson(id));
+    }
+
+    @PatchMapping(path = "/{id}")
+    public void updatePerson(@PathVariable long id, @RequestBody Person person) {
+        Person findPerson = getPerson(id);
+        findPerson.setFirstName(person.getFirstName());
+        findPerson.setLastName(person.getLastName());
+        this.personRepository.save(findPerson);
+    }
+    // END
+}
