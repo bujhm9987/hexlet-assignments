6358dd761b9eee2cf89b888595bd416e9cb188ce
@hexlet/cli <support@hexlet.io>
2023-10-17 21:50:58 +0300

    submit java-spring-ru/filtration

--- java-spring-ru/filtration/src/main/java/exercise/controller/UsersController.java
+++ java-spring-ru/filtration/src/main/java/exercise/controller/UsersController.java
@@ -51,6 +51,42 @@ public class UsersController {
                     return specificationResult.and(specification);
                 }));
     }
+
+    //    Альтернативное решение с использованием Querydsl (дополнительная задача)
+//    if (firstName == null && lastName == null) {
+//        return userRepository.findAll();
+//    }
+//
+//        if (firstName == null) {
+//        return userRepository.findAll(
+//                QUser.user.lastName.containsIgnoreCase(lastName)
+//        );
+//    }
+//
+//        if (lastName == null) {
+//        return userRepository.findAll(
+//                QUser.user.firstName.containsIgnoreCase(firstName)
+//        );
+//    }
+//
+//        return userRepository.findAll(
+//                QUser.user.firstName
+//                        .containsIgnoreCase(firstName)
+//            .and(
+//            QUser.user.lastName
+//                    .containsIgnoreCase(lastName)
+//                )
+//                        );
+
+
+
+
+//     Альтернативное решение с использованием Querydsl web support (дополнительная задача)
+//
+//     @GetMapping(path = "")
+//     public Iterable<User> getUsers(@QuerydslPredicate(root = User.class) Predicate predicate) {
+//         return userRepository.findAll(predicate);
+//     }
     // END
 }
 
